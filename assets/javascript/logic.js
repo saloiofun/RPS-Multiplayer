2d0f837abe708757e4d7184d41400354d85088ec
Sandro Wong <saloiofun@gmail.com>
2017-07-29 21:46:18 -0700

    create layout and players logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,38 +11,116 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
 
+  // Player's constructor
+  function Player(name, wins, losses) {
+    this.name = name;
+    this.wins = wins;
+    this.losses = losses;
+  }
+
   // Initialize variables
   var database = firebase.database();
-  var wins = 0;
-  var losses = 0;
+  var playerData;
   
-  function writeUserData(userId, name, wins, losses) {
-  	database.ref("/player " + userId).set({
-  		name: name,
-  		wins: wins,
-  		losses: losses
+  // Write player's information to Database
+  var writeUserData = function(player) {
+  	database.ref(playerData).set({
+  		name: player.name,
+  		wins: player.wins,
+  		losses: player.losses
   	});
   }
 
-  function writeUserChoice(userId, choice) {
-  	database.ref("/player " + userId).update({
+  // Write to player's choice to Database 
+  var writeUserChoice = function(player, choice) {
+  	database.ref(player).update({
   		choice: choice
   	});
   }
 
-  $("#rock").on("click", function() {
-  	writeUserChoice(1, "Rock");
+  // Remove player from Database 
+  var removeUserOnDisconnect = function() {
+    var connectedRef = database.ref(".info/connected");
+    connectedRef.on("value", function(snap) {
+      // Checks if player is in Database
+      if (snap.val()) {
+        var con = database.ref().child(playerData);
+        con.onDisconnect().remove();
+      }
+    });
+  }
+
+  // Function to update the players stats (wins, losses)
+  var playerStats = function (obj, element) {
+    var name = $("<p>").text(obj.name);
+    var wins = $("<p>").text("Wins: " + obj.wins);
+    var losses = $("<p>").text("Losses: " + obj.losses);
+
+    $(element).empty();
+    $(element).append(name);
+    $(element).append(wins);
+    $(element).append(losses);
+  };
+
+  $("#name-submit").on("click", function (event) {
+    event.preventDefault();
+
+    var playerName = $("#player-name").val().trim();
+
+    if (playerName) {
+
+      var player = new Player(playerName, 0, 0);
+
+      database.ref().once("value", function(snapshot) {
+        var hasPlayerOne = snapshot.child("/player 1").exists();
+        var hasPlayerTwo = snapshot.child("/player 2").exists();
+
+        if ((!hasPlayerOne && !hasPlayerTwo) || (!hasPlayerOne && hasPlayerTwo))  {
+          $("#name-header").html("<h1>Hi " + playerName + "! You are player 1</h1>");
+          playerData = "/player 1";
+          writeUserData(player);
+
+          var playerRef = database.ref(playerData);
+          playerRef.on("value", function(snapshot) {
+            player.wins = snapshot.val().wins;
+            player.losses = snapshot.val().losses;
+            playerStats(player, "#player-one-stats");
+          })
+
+          removeUserOnDisconnect();
+
+        } 
+        if (hasPlayerOne && !hasPlayerTwo) {
+          $("#name-header").html("<h1>Hi " + playerName + "! You are player 2</h1>");
+          playerData = "/player 2";
+          writeUserData(player);
+
+          var playerRef = database.ref(playerData);
+          playerRef.on("value", function(snapshot) {
+            player.wins = snapshot.val().wins;
+            player.losses = snapshot.val().losses;
+            playerStats(player, "#player-two-stats");
+          })
+
+          removeUserOnDisconnect();
+        }
+
+      });
+    }
+
   });
 
-  $("#paper").on("click", function() {
-  	writeUserChoice(1, "Paper");
+  $(".rps").on("click", function() {
+    var choice = $(this).attr("data-choice");
+    if (choice === "rock") { 
+      writeUserChoice(playerData, "Rock") 
+    };
+    if (choice === "paper") { 
+      writeUserChoice(playerData, "Paper") 
+    };
+    if (choice === "scissors") { 
+      writeUserChoice(playerData, "Scissors") 
+    };
   });
-
-  $("#scissors").on("click", function() {
-  	writeUserChoice(1, "Scissors");
-  });
-
-  writeUserData(1, "Sandro", wins, losses);
-
 
 });
